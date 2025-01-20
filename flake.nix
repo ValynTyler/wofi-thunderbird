d0be59f15ba7c3cc8d8ba35e74f7677b30958e6f
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-20 21:50:18 +0200

    feat: added nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    packages.x86_64-linux.default = self.packages.x86_64-linux.wofirefox;
+    packages.x86_64-linux.wofirefox =
+      pkgs.stdenv.mkDerivation {
+        name = "wofirefox";
+        src = self;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ./wofirefox.sh $out/bin/wofirefox
+          chmod +x $out/bin/wofirefox
+        '';
+      };
+  };
+}
