6f9757964a8e26cdcd8fe1488cf8e6b21843f7d1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-20 22:12:28 +0200

    fix: update wofimail.sh

--- wofiemail.sh
+++ wofiemail.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CONFIG_PATH=~/.mozilla/firefox/profiles.ini
+CONFIG_PATH=~/.thunderbird/profiles.ini
 
 choice=$(awk -F "=" '/Name/ { print $2 }' $CONFIG_PATH | sort | wofi --dmenu --prompt "Select a profile...")
 
 if [ "$choice" != "" ]; then
-    firefox -P $choice &
+    thunderbird -P $choice &
 else
     echo "closed with no selection."
     false
